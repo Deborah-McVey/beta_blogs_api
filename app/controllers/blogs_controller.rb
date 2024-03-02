7ac3333e310f0d5adbb6b684a7ed8b8010f367bd
Deborah McVey <deborah.mcvey@gmail.com>
2024-03-02 13:19:33 -0500

    fullstack project part 3 token interceptor

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,8 +8,7 @@ class BlogsController < ApplicationController
     end
   
     def create 
-      blog = Blog.new(blog_params)
-  
+      blog = @current_user.blogs.new(blog_params)
       
       if blog.save
         render json: BlogBlueprint.render(blog, view: :normal), status: :created
@@ -21,7 +20,7 @@ class BlogsController < ApplicationController
     private 
   
     def blog_params 
-      params.permit(:title, :content, :user_id)
+      params.permit(:title, :content)
     end
-  
   end
+  
